976b106f040ca823a0ef1883a27b8dcdb2bcdf30
anayksha <anaykshatriya@gmail.com>
2025-07-23 11:53:52 -0400

    made Vector more efficient

--- physics.py
+++ physics.py
@@ -11,51 +11,31 @@ class Vector:
         if input_angle is False, val1 and val2 are x and y components, respectively
         '''
         if input_angle:
-            self._angle = val2
-            self.magnitude = val1
+            # TODO
+            pass
         else:
-            self._x = val1
+            self.x = val1
             self.y = val2
 
     @property
     def magnitude(self):
-        return self._magnitude
+        return math.hypot(self.x, self.y)
 
     @property
     def angle(self):
-        return self._angle
-
-    @property
-    def x(self):
-        return self._x
-
-    @property
-    def y(self):
-        return self._y
+        return math.atan2(self.y, self.x)
 
     @magnitude.setter
     def magnitude(self, magnitude):
-        self._magnitude = magnitude
-        self._x = self.magnitude * math.cos(self.angle)
-        self._y = self.magnitude * math.sin(self.angle)
+        angle = self.angle
+        self.x = magnitude * math.cos(angle)
+        self.y = magnitude * math.cos(angle)
 
     @angle.setter
     def angle(self, angle):
-        self._angle = angle
-        self._x = self.magnitude * math.cos(self.angle)
-        self._y = self.magnitude * math.sin(self.angle)
-
-    @x.setter
-    def x(self, x):
-        self._x = x
-        self._magnitude = math.hypot(self.x, self.y)
-        self._angle = math.atan2(self.y, self.x)
-
-    @y.setter
-    def y(self, y):
-        self._y = y
-        self._magnitude = math.hypot(self.x, self.y)
-        self._angle = math.atan2(self.y, self.x)
+        magnitude = self.magnitude
+        self.x = magnitude * math.cos(angle)
+        self.y = magnitude * math.sin(angle)
 
     def __add__(self, other:"Vector") -> "Vector":
         return Vector(self.x + other.x, self.y + other.y)
